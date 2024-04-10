d74af10c7338aa35e9fc4d134100ae54c9cebdad
Devilal panchal <devilalpanchal551@gmail.com>
2024-04-10 22:05:03 +0530

    update js

---
+++ index.js
@@ -0,0 +1,71 @@
+const ShortInput = document.getElementById('ShortInput')
+const ShortBtn = document.getElementById('ShortBtn')
+const CreateLinkSection = document.getElementById('CreateLinkSection')
+
+async function short() {
+    let value = ShortInput.value;
+    const response = await fetch(`https://tinyurl.com/api-create.php?url=${value}`);
+    let data = await response.text(); //.json() is not working
+    console.log(data);
+    const Element = document.createElement('section')
+    Element.classList.add('linkSection')
+
+    const div = document.createElement('div')
+    div.classList.add('linkMain')
+    Element.append(div)
+
+    const div1 = document.createElement('div')
+    div1.classList.add('linkShare')
+    div.appendChild(div1)
+
+    const span = document.createElement('span')
+    span.classList.add('linkInput')
+    div1.appendChild(span)
+    span.textContent = ShortInput.value;
+
+    const span1 = document.createElement('span')
+    const ahref = document.createElement('a')
+    ahref.classList.add('linkColor')
+    ahref.href =  data;
+    ahref.textContent =  data;
+    span1.appendChild(ahref)
+    div1.appendChild(span1)
+
+    const btn = document.createElement('button')
+    btn.classList.add('linkBtn')
+    btn.textContent = 'Copy'
+    div.appendChild(btn)
+    CreateLinkSection.append(Element)
+
+    console.log(Element);
+
+
+
+}
+
+
+ShortBtn.addEventListener('click', () => {
+    short()
+
+})
+
+// to copy link
+btn.addEventListener('click',()=>{
+    btn.classList.add('linkBtn1')
+})
+
+// const copyBtnAll = [...document.querySelectorAll('.linkBtn')]
+// copyBtnAll.forEach(ele => {
+   
+//     ele.addEventListener('click', (e) => {
+//         copyBtnAll.forEach(ele => {
+//             ele.textContent = 'Copy'
+//             ele.style.backgroundColor = 'hsl(180, 66%, 49%)'
+//         })
+//         copyTextToclipborad(shortLink)
+//         e.target.textContent = "Copied!"
+//         e.target.style.backgroundColor = 'hsl(257, 27%, 26%)'
+//     })
+
+// }) 
+
